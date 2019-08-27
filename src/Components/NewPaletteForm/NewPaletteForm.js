57c49b5f03de4aa17340dc5ba98abcdf85fda1bf
DeMarcus Kirby <tkd024@gmail.com>
2019-08-27 15:07:03 -0600

    Add post functionality for projects
    Co-Authored-By: Kayla Larson <kayla.larson1990@gmail.com>

--- src/Components/NewPaletteForm/NewPaletteForm.js
+++ src/Components/NewPaletteForm/NewPaletteForm.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux'
 import './NewPaletteForm.css'
 
 export class NewPaletteForm extends Component {
@@ -6,7 +7,8 @@ export class NewPaletteForm extends Component {
     super(props);
     this.state = {
       form: false,
-      palatte_name: ''
+      project_name: '',
+      palette_name: ''
     }
   }
 
@@ -16,21 +18,38 @@ export class NewPaletteForm extends Component {
 
   handleChange = e => {
     this.setState({
-      palatte_name: e.target.value
+      palette_name: e.target.value
     })
+  }
+
+  updateProject = e => {
+    this.setState({ project_name: e.target.value})
   }
 
   handleSubmit = e => {
     e.preventDefault();
+    let projId = this.props.projects.filter(project => {
+      if(project.project_name === this.state.project_name){
+        return project.id
+      }
+    })
+    this.props.addNewPalette({palette: this.state.palette_name, project: projId[0].id})
     this.setState({ form: false})
   }
 
   render() {
+    let projects = this.props.projects.map(project => {
+      return <option>{project.project_name}</option>
+    })
     return (
       <section>
       {!this.state.form && <button className='toggle-form' onClick={e => this.populateForm()}>Save palette</button>}
       { this.state.form && 
         <form>
+          <select onChange={e => this.updateProject(e)}>
+            <option>SELECT PROJECT</option>
+            {projects}
+          </select>
           <input className='palette-name' placeholder='Enter Palatte Name...' value={this.state.palatte_name} onChange={e => this.handleChange(e)}/>
           <button onClick={e => this.handleSubmit(e)}>Submit</button>
         </form>
@@ -40,4 +59,8 @@ export class NewPaletteForm extends Component {
   }
 }
 
-export default NewPaletteForm;
+const mapStateToProps = store => ({
+  ...store
+})
+
+export default connect(mapStateToProps)(NewPaletteForm);
