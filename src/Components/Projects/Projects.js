b91ca28164c7bb931e84ad7ee6bcdd602146bae3
DeMarcus Kirby <tkd024@gmail.com>
2019-08-28 15:50:00 -0600

    Add ability to delete project and associated palettes

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Palette from '../Pallete/Palette'
 import './Projects.css'
+import { deleteProject, fetchAllProjects, fetchAllPalettes } from '../../apiCalls';
+import { gatherProjects, gatherPalettes } from '../../actions';
 
 const Projects = (props) => {
   let paletteList = props.palettes.map(palette => {
@@ -12,9 +14,21 @@ const Projects = (props) => {
     }
   })
 
+  const handleDelete = async id => {
+    await deleteProject({id})
+    let projects = await fetchAllProjects()
+    props.removeProject(projects)
+    let palettes = await fetchAllPalettes()
+    props.removePalette(palettes)
+  }
+
   return(
     <section className='project-section'>
-      <h3>{props.name}</h3>
+    
+      <div className='project-info'>
+        <h3>{props.name}</h3>
+        <p onClick={e => handleDelete(props.id)}>🗑</p>
+      </div>
       <section>
         {paletteList}
       </section>
@@ -26,4 +40,9 @@ const mapStateToProps = store => ({
   palettes: store.palettes
 })
 
-export default connect(mapStateToProps)(Projects);
+const mapDispatchToProps = dispatch => ({
+  removeProject: projects => dispatch(gatherProjects(projects)),
+  removePalette: palettes => dispatch(gatherPalettes(palettes))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Projects);
